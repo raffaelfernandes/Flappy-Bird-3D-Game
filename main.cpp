7b04bf89b6fecdf0402b107f711e3c764d67b5d4
Raffael Fernandes <103156628+raffaelfernandes@users.noreply.github.com>
2024-02-04 10:11:39 -0300

    Chaotic Mode now working

--- main.cpp
+++ main.cpp
@@ -30,7 +30,36 @@ double newJumpStrength = jumpStrength;
 double decremento = -0.02f;
 
 int score = 0;
+int scoreScreen = 0;
+int chaosSeed = 0;
+int stopScore = 0;
+int chaosDuration = 0;
+int stopChaos = 0;
+
 bool gameOver = false;
+bool chaosMode = false;
+bool change = false;
+
+//
+// Armazenando os modos de iluminacao
+float globalAmb0[] = {0.5f, 0.5f, 0.5f, 1.f};
+    
+float light0[4][4] = {
+{0.1f, 0.1f, 0.1f, 1.f}, // ambiente
+{0.8f, 0.8f, 0.8f, 1.f}, // diffuse
+{1.f, 1.f, 1.f, 1.f}, // specular
+{3.0f, 1.0f, 2.0f, 1.f} // position
+};
+	
+float globalAmb1[] = {0.05f, 0.05f, 0.05f, 0.5f};
+float light1[4][4] = {
+		{0.05f, 0.05f, 0.05f, 0.5f}, // ambiente
+		{0.8f, 0.8f, 0.8f, 1.f}, // diffuse
+		{1.f, 1.f, 1.f, 1.f}, // specular
+		{12.0f, 1.0f, 0.0f, 1.f} // position
+		};
+//
+//
 
 float gap = 1.2;
 float reshape_ratio = 1.0; // Manter proporcional a projecao
@@ -49,6 +78,7 @@ LinkedList<Pipe> pipeList;
 int main(int argc, char** argv) {
     // Inicializa a semente para a função rand() usando o tempo atual
     std::srand(static_cast<unsigned int>(std::time(NULL)));	
+    chaosSeed = randomNumber(8, 16);
 	
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -81,15 +111,8 @@ void init(void){
     glEnable(GL_LIGHT0);
     
     glEnable(GL_COLOR_MATERIAL);
-    float globalAmb[] = {0.5f, 0.5f, 0.5f, 1.f};
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmb);
     
-    float light0[4][4] = {
-	{0.1f, 0.1f, 0.1f, 1.f}, // ambiente
-	{0.8f, 0.8f, 0.8f, 1.f}, // diffuse
-	{1.f, 1.f, 1.f, 1.f}, // specular
-	{3.0f, 1.0f, 2.0f, 1.f} // position
-	};
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmb0);
     
     glLightfv(GL_LIGHT0, GL_AMBIENT, &light0[0][0]);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, &light0[1][0]);
@@ -128,22 +151,32 @@ void keyboard (unsigned char key, int x, int y){
     }
 }
 
-void drawBackground(){
-	glPushMatrix();
-		glTranslatef(0.0, -0.8f, -2.0);
-		glColor3f(0.56f, 0.44f, 0.32f);
-		glBegin(GL_POLYGON);
-			glVertex3f(-7.f, -3.f, 0.f);
-			glVertex3f(-5.f, -1.f, 0.f);
-			glVertex3f(-4.5f, 0.f, 0.f);
-			glVertex3f(-2.0f, 1.2f, 0.f);
-			glVertex3f(0.7f, 1.3f, 0.f);
-			glVertex3f(1.2f, -0.5f, 0.f);
-			glVertex3f(1.7f, 0.5f, 0.f);
-			glVertex3f(1.8f, 0.6f, 0.f);
-			glVertex3f(6.0f, -3.f, 0.f);
-		glEnd();
-	glPopMatrix();
+void drawBackground(bool chaosMode){
+	if(!chaosMode){
+		glPushMatrix();
+			glTranslatef(0.0, -0.8f, -2.0);
+			glColor3f(0.28f, 0.22f, 0.16f);
+			glBegin(GL_POLYGON);
+				glVertex3f(-7.f, -3.f, 0.f);
+				glVertex3f(-5.f, -1.f, 0.f);
+				glVertex3f(-4.5f, 0.f, 0.f);
+				glVertex3f(-2.0f, 1.2f, 0.f);
+				glVertex3f(0.7f, 1.3f, 0.f);
+				glVertex3f(1.2f, -0.5f, 0.f);
+				glVertex3f(1.7f, 0.5f, 0.f);
+				glVertex3f(1.8f, 0.6f, 0.f);
+				glVertex3f(6.0f, -3.f, 0.f);
+			glEnd();
+		glPopMatrix();
+		return;
+	}
+	glColor3f(0.02f, 0.14f, 0.02f);
+	glBegin(GL_QUADS);
+		glVertex3f(-7.0f, 7.0f, -3.0f);
+		glVertex3f(7.0f, 7.0f, -3.0f);
+		glVertex3f(7.0f, -7.0f, -3.0f);
+		glVertex3f(-7.0f, -7.0f, -3.0f);
+	glEnd();
 	
 	//SOL
 	// glPushMatrix();
@@ -166,6 +199,27 @@ void drawBackground(){
 	// glPopMatrix();
 }
 
+void changeLighting(bool chaosMode){
+	if (chaosMode){
+	    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmb1);
+	    
+	    glLightfv(GL_LIGHT0, GL_AMBIENT, &light1[0][0]);
+	    glLightfv(GL_LIGHT0, GL_DIFFUSE, &light1[1][0]);
+	    glLightfv(GL_LIGHT0, GL_SPECULAR, &light1[2][0]);
+	    glLightfv(GL_LIGHT0, GL_POSITION, &light1[3][0]);
+		change = !change;
+		return;
+	}
+
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmb0);
+    
+    glLightfv(GL_LIGHT0, GL_AMBIENT, &light0[0][0]);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, &light0[1][0]);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, &light0[2][0]);
+    glLightfv(GL_LIGHT0, GL_POSITION, &light0[3][0]);
+    change = !change;
+    return;
+}
 
 void display(void){
 	// Apaga o video e o depth buffer, e reinicia a matriz
@@ -178,15 +232,19 @@ void display(void){
     glLoadIdentity();
 
     // Desenha o jogador e os obstáculos
-	drawBackground();
-    
+    if(change){
+		changeLighting(chaosMode);
+	}
+    drawBackground(chaosMode);
     player.drawPlayer();
 	pipeList.drawList(decremento);
 	
 	glPushMatrix();
 		// Desenhando textos na janela GLUT
 	    glDisable(GL_LIGHTING); // Desliga a Luz para desenhar o texto
-	    glColor3f(0.0, 0.0, 0.0); // seleciona a cor preta para o texto
+	    if(chaosMode) glColor3f(1.0, 1.0, 1.0);
+	    if(!chaosMode) glColor3f(0.0, 0.0, 0.0);
+	     // seleciona a cor para o texto
 	    float x = -reshape_ratio-0.45;
 	    glTranslatef(0.0, 0.0, 3.0);
 	    draw_text_stroke(x-0.5, 1.25, "SCORE: " + to_string(score));
@@ -216,12 +274,26 @@ void update(int value) {
 				pipeList.dequeue(pipe);
 				pipeList.enqueue(Pipe(6.5, randomNumber(-2.0, 2.0), 0, gap));
 				score++;
-				if(score > 0 && score%3  == 0){
+				if(score > 0 && score%4  == 0){
 					newJumpStrength += newJumpStrength*0.02;
 					gravity += gravity*0.02;
 					decremento += decremento*0.06;
 					
 				}
+				// chama o chaosMode
+				if(score%chaosSeed == 0 && score >= (stopChaos + 2.5*chaosDuration)){
+					chaosMode = true;
+					change = true;
+					chaosDuration = randomNumber(5, 11);
+					stopChaos = score + chaosDuration;
+				}
+				if(score > stopChaos){
+					chaosMode = false;
+					change = true;
+					chaosSeed = randomNumber(8, 15);
+				}
+				
+				
 			}
 			if (xEP <= (pipe.getXCoord()+1.0) && xDP >= pipe.getXCoord()){
 				if (((player.getYCoord()) <= pipe.getYCoord()-(gap/2)) || (player.getYCoord()) >= pipe.getYCoord()+ (gap/2)){
